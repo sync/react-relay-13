36cb61ce939af3d30eaf33bc5c5e6e5898e4c9a5
Anthony Mittaz <sync@me.com>
2021-12-04 15:29:14 +1100

    chore: setup films query

---
+++ src/__generated__/AppQuery.graphql.ts
@@ -0,0 +1,108 @@
+/**
+ * @generated SignedSource<<957fa3b8097ec77971cae0961195a90e>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ConcreteRequest, Query } from 'relay-runtime';
+export type AppQuery$variables = {};
+export type AppQuery$data = {
+  readonly allFilms: {
+    readonly edges: ReadonlyArray<{
+      readonly node: {
+        readonly id: string;
+        readonly title: string | null;
+      } | null;
+    } | null> | null;
+  } | null;
+};
+export type AppQuery = {
+  variables: AppQuery$variables;
+  response: AppQuery$data;
+};
+
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "alias": null,
+    "args": null,
+    "concreteType": "FilmsConnection",
+    "kind": "LinkedField",
+    "name": "allFilms",
+    "plural": false,
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "FilmsEdge",
+        "kind": "LinkedField",
+        "name": "edges",
+        "plural": true,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "Film",
+            "kind": "LinkedField",
+            "name": "node",
+            "plural": false,
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "kind": "ScalarField",
+                "name": "id",
+                "storageKey": null
+              },
+              {
+                "alias": null,
+                "args": null,
+                "kind": "ScalarField",
+                "name": "title",
+                "storageKey": null
+              }
+            ],
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ],
+    "storageKey": null
+  }
+];
+return {
+  "fragment": {
+    "argumentDefinitions": [],
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "AppQuery",
+    "selections": (v0/*: any*/),
+    "type": "Root",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": [],
+    "kind": "Operation",
+    "name": "AppQuery",
+    "selections": (v0/*: any*/)
+  },
+  "params": {
+    "cacheID": "e5ddefbaaaa9cc34f6780c1e2478e883",
+    "id": null,
+    "metadata": {},
+    "name": "AppQuery",
+    "operationKind": "query",
+    "text": "query AppQuery {\n  allFilms {\n    edges {\n      node {\n        id\n        title\n      }\n    }\n  }\n}\n"
+  }
+};
+})();
+
+(node as any).hash = "eb5af8226a95992e7e940e1de5f8a65c";
+
+export default node;
